12ecd5d99a2373b23070390122a6b147a99d8b11
cgjgh <160297365+cgjgh@users.noreply.github.com>
2024-07-29 16:47:58 -0500

    Add more user info to align with Flowfuse user auth plugin

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+function getUser(headers) {
+  var user = {};
+  const authentik_user = headers["x-authentik-uid"] || null;
+  if (!authentik_user) {
+    console.warn(`Session is not authenticated by Authentik; no user detected. See headers: ${JSON.stringify(headers)}`);
+  } else {
+    console.log(`Dashboard interacted with by ${authentik_user}`);
+  }
+  user.host = headers["host"] || null;
+  user.agent = headers["user-agent"] || null;
+  user.userId = authentik_user;
+  user.name = headers["x-authentik-name"] || null;
+  user.userName = headers["x-authentik-username"] || null;
+  user.email = headers["x-authentik-email"] || null;
+  user.groups = headers["x-authentik-groups"] || null;
+  user.provider = "Authentik";
+  return user;
+}
+
 let plugin_name = "node-red-dashboard-2-authentik-auth";
 module.exports = function (RED) {
   RED.plugins.registerPlugin(plugin_name, {
@@ -5,46 +24,51 @@ module.exports = function (RED) {
     type: "node-red-dashboard-2",
 
     hooks: {
-      /**
-       * onAddConnectionCredentials - called when a D2.0 is about to send a message in Node-RED
-       * @param {object} conn - SocketIO connection object
-       * @param {object} msg - Node-RED msg object
-       * @returns {object} - Returns Node-RED msg object
-       */
+      onSetup: (config, req, res) => {
+        var user = getUser(req.headers);
+        return {
+          socketio: {
+            path: `${config.path}/socket.io`,
+            withCredentials: true,
+            auth: {
+              user: user
+            }
+          }
+        }
+      },
+
       onAddConnectionCredentials: (conn, msg) => {
         if (!msg._client) {
           console.log(
             `${plugin_name}: msg._client is not found, not adding user info. This sometimes happens when the editor is refreshed with stale connections to the dashboard.`
           );
+          // msg._client = {}  // flowfuse does this and continues execution to set user info below, not sure why.
           return msg;
         }
-        var user = {};
-        const headers = conn.request.headers;
-        // Just for debugging ... 
-        // console.warn(`${JSON.stringify(headers)}`)
-        const authentik_user = headers["x-authentik-uid"] || null
-        if (!authentik_user) {
-          console.warn(
-            `${plugin_name}: Session is not authenticated by Authentik; no user detected. See headers: ${JSON.stringify(
-              headers
-            )}`
-          );
-        } else {
-          console.log(
-            `${plugin_name}: Dashboard interacted with by ${authentik_user}`
-          );
-        }
-        user.host = headers["host"] || null;
-        user.agent = headers["user-agent"] || null;
-        user.userId =  authentik_user
-        user.name =  headers["x-authentik-name"] || null;
-        user.userName =  headers["x-authentik-username"] || null;
-        user.email = headers["x-authentik-email"] || null
-        user.groups =  headers["x-authentik-groups"] || null;
-        user.provider = "Authentik";
+
+        var user = getUser(conn.request.headers);
         msg._client["user"] = user;
         return msg;
       },
+
+      onIsValidConnection(conn, msg) {
+        // check if the msg contains a specified user
+        // if it does, does that user match the user in the connection?
+        if (msg._client?.user) {
+          var user = getUser(conn.request.headers);
+          return msg._client.user.userId === user?.userId
+        }
+        return true
+      },
+
+      onCanSaveInStore(msg) {
+        // check if the msg contains a specified user
+        // if it does, then we can't save in general data store
+        if (msg._client?.user) {
+          return false
+        }
+        return true
+      }
     },
   });
 };
